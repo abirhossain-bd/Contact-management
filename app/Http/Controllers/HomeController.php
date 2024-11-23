031ff4ad410e00ac81eeaf1ff3291c43c9e667ed
Abir <mdabirhossain.bd.info@gmail.com>
2024-11-23 03:04:58 -0600

    join query

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index(){
-        $user = Auth::user();
+        $user = User::join('cities','cities.id','users.city_id')->where('users.id',Auth::id())->select('users.*','cities.name')->first();
+
         if (!$user) {
             return redirect()->route('login');
         }
